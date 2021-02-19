53e82e634ed6eb6572f403dad62d126d953c2924
Alex <Alex@DESKTOP-T4COF2N>
2021-02-18 20:29:59 -0600

    primer commit

---
+++ src/git57297/Git57297.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git57297;
+
+/**
+ *
+ * @author Alex
+ */
+public class Git57297 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = 'Alexander';
+        String matricula = '57297';
+        
+        System.out.println("Soy " + nombre 'mi matricula es ' + matricula);
+        
+        int sueldo =1000;
+    }
+    
+}
